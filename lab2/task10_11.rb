709cfe01f6b571d4f38e21b77615cb787d06fbdd
DeadDDolphin <dimonych19@gmail.com>
2021-04-24 23:56:43 +0300

    Task 11 - method 1

--- lab2/task10_11.rb
+++ lab2/task10_11.rb
@@ -9,9 +9,13 @@ def find_date(str)
   return str.scan(/(?:[1-9]|[12][\d]|[3][01]) (?:декабря|января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября) (?:[1-9]{1}\d*)/)
 end
 
+def kirill(str)
+  return str.scan(/[А-я]+/).sort_by{|el| el.size}.pop.size
+end
+
 puts "Выберите какое задание хотите решить."
 puts "1 - найти все даты вида 00 месяц 0000"
-puts "2 - "
+puts "2 - найбольшее кол-во идущих подряд символов кириллицы"
 puts "3 - "
 puts "4 - "
 
@@ -19,10 +23,13 @@ choose = input.to_i
 
 case choose
 when 1
+  puts "Введите строку с датами"
   str = input
   puts find_date(str) 
 when 2
-  pass
+  puts "Введите сроку с кириллицей"
+  str = input
+  puts kirill(str)
 when 3
   pass
 when 4
