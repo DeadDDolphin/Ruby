53e9c8868850978b6a478cf62fb523554fbbc71e
DeadDDolphin <dimonych19@gmail.com>
2021-04-24 18:55:20 +0300

    Second method

--- lab2/task3.rb
+++ lab2/task3.rb
@@ -3,7 +3,16 @@ def read1(size)
   return STDIN.gets.chomp().split().reduce([]){|arr, el| arr.push(el.to_i)}
 end
 
+def read2(size)
+  print "Введите массив чисел, #{size} штук: "
+  arr = STDIN.gets.chomp().split()
+  new_arr = Array.new()
+  for i in 0..size-1
+    new_arr[i] = arr[i].to_i
+  end
+  return new_arr
+end
 size = ARGV[0].to_i
 
-print (read1(size))
+print (read2(size))
 print "\n"
