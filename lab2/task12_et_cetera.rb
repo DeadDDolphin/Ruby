3bc2122fcc5ddfa8361a57c12f8f8c4a7d0dee1b
DeadDDolphin <dimonych19@gmail.com>
2021-04-26 01:29:25 +0300

    Ну тип следующее... -_-

--- lab2/task12_et_cetera.rb
+++ lab2/task12_et_cetera.rb
@@ -17,8 +17,22 @@ def order_by_words_count(arr)
     |str| str.scan(/[[:word:]]+/).length
   }
 end
+
+#АААААА, я не понимаю, как по мне, эти регулярки должны находить только слова, идущие после чисел. 
+#А оно находит абсолютно всё, кроме пунктуации. 
+#Я в бешенстве уже от этого, так что забью, успокоюсь, буду другое делать
+def order_by_count_of_words_after_number(arr)
+  arr.each do |str|
+    print str[/[^\d+\s*[:word:]$]/]
+    print "\n"
+  end
+  arr.sort_by{
+   |str| str.scan(/[^\d+\s*[:word:]$]+/).length 
+  }
+end
 path = ARGV[0]
 
 arr = read_file(path)
 #puts order_by_length(arr)
-puts order_by_words_count(arr)
+#puts order_by_words_count(arr)
+#puts order_by_count_of_words_after_number(arr)
