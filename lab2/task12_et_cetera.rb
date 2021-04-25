a61b356d74147125c02abfa111f7f3c144ee948e
DeadDDolphin <dimonych19@gmail.com>
2021-04-25 23:48:16 +0300

    Task 12
    Read list of strings from file and sort them by length

---
+++ lab2/task12_et_cetera.rb
@@ -0,0 +1,18 @@
+def read_file(path)
+  f = open path
+  arr=Array.new()
+  f.each do |line|
+    arr<<line
+  end
+  f.close
+  return arr
+end
+
+def order_by_length(arr)
+   arr.sort_by{|el| el.size}
+end
+
+path = ARGV[0]
+
+arr = read_file(path)
+puts order_by_length(arr)
