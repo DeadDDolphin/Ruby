7251db21ed444eaaf5ab43b77366439a355a7de6
DeadDDolphin <dimonych19@gmail.com>
2021-04-26 00:01:02 +0300

    Task 13
    Order by count if words in every string

--- lab2/task12_et_cetera.rb
+++ lab2/task12_et_cetera.rb
@@ -12,7 +12,13 @@ def order_by_length(arr)
    arr.sort_by{|el| el.size}
 end
 
+def order_by_words_count(arr)
+  arr.sort_by{
+    |str| str.scan(/[[:word:]]+/).length
+  }
+end
 path = ARGV[0]
 
 arr = read_file(path)
-puts order_by_length(arr)
+#puts order_by_length(arr)
+puts order_by_words_count(arr)
