c4478a8c82fbf68415377155107a173484a3a323
DeadDDolphin <dimonych19@gmail.com>
2021-05-16 15:08:42 +0300

    So, i change my class. Now it's more beauty

--- lab3/Employee.rb
+++ lab3/Employee.rb
@@ -1,47 +1,57 @@
 class Employee
   attr_accessor :fio, :birth_date, :phone_number, :adress, :mail, :pasport_serial, :speciality, :expirience, :last_place_of_job, :last_job, :last_zarplata
-  
+
   def initialize(data)
-    @fio = data[0]
-    @birth_date = data[1]
-    @phone_number = data[2]
-    @adress = data[3]
-    @mail = data[4]
-    @pasport_serial = data[5]
-    @speciality = data[6]
-    @expirience = data[7].to_i
-    if @expirience > 0  
-      @last_place_of_job = data[8]
-      @last_job = data[9]
-      @last_zarplata = data[10]
+    set_all(data)
+  end
+
+  def last_place_of_job=(place)
+    if @expirience > 0
+      @last_place_of_job = place
+    else
+      @last_place_of_job = "No"
     end
   end
-  
+
+  def last_job=(job)
+    if @expirience > 0
+      @last_job = job
+    else
+      @last_job = "No"
+    end
+  end
+
+  def last_zarplata=(zarplata)
+    if @expirience > 0
+      @last_zarplata = zarplata
+    else
+      @last_zarplata = "No"
+    end
+  end
+
   def set_all(data)
-    @fio = data[0]
-    @birth_date = data[1]
-    @phone_number = data[2]
-    @adress = data[3]
-    @mail = data[4]
-    @pasport_serial = data[5]
-    @speciality = data[6]
-    @expirience = data[7].to_i
-    if @expirience > 0  
-      @last_place_of_job = data[8]
-      @last_job = data[9]
-      @last_zarplata = data[10]
-    end
+    self.fio = data[0]
+    self.birth_date = data[1]
+    self.phone_number = data[2]
+    self.adress = data[3]
+    self.mail = data[4]
+    self.pasport_serial = data[5]
+    self.speciality = data[6]
+    self.expirience = data[7].to_i
+    self.last_place_of_job = data[8]
+    self.last_job = data[9]
+    self.last_zarplata = data[10]
   end
-  
-  def get_all()
+
+  def get_all
     return @fio, @birth_date, @phone_number, @adress, @mail, @pasport_serial,
-  @speciality, @expirience, @last_place_of_job, @last_job, @last_zarplata
+      @speciality, @expirience, @last_place_of_job, @last_job, @last_zarplata
   end
-  
-  def employee()
-    return @fio  
+
+  def to_s
+    return @fio + "; place: " + @last_place_of_job
   end
-  
+
   def change_job(place, job, money)
     if @expirience > 0
       @last_place_of_job = place
@@ -53,39 +63,49 @@ class Employee
   end
 end
 
-class TestEmployee < Employee
-  def employee
-    return "Человеку по имени " + @fio + " звони сюда " + @phone_number
+class TestEmployee
+  def read_from_file(path)
+    f = open path
+    arr = Array.new
+    f.each{|line| arr << line.chomp()}
+    f.close
+    return arr
+  end
+
+  def input
+    return STDIN.gets.chomp()
+  end
+
+  def get_data(choose)
+    case choose
+    when "1"
+      path = "data_list.txt"
+      data = read_from_file(path)
+    when "2"
+      puts "Введите данные работника: "
+      data = input
+    end
+    return data
+  end
+
+  def create_employees(data)
+    emps = Array.new
+    for i in 0..data.length/11 - 1
+      emps<< Employee.new(data[i*11..i*11+10])
+    end
+    return emps
+  end
+
+  def new_employee(data)
+    return Employee.new(data)
+  end
+
+  def run(choose)
+    data = get_data(choose)
+    emps = create_employees(data)
+    emps.each{|el| puts el}
   end
 end
 
-def read_from_file(path)
-  f = open path
-  arr=Array.new()
-  f.each do |line|
-    arr<<line.chomp()
-  end
-  f.close
-  return arr
-end
-
-def input()
-  return STDIN.gets.chomp()
-end
-
-choose = ARGV[0]
-
-case choose
-when "1"
-  path = ARGV[1]
-  data = read_from_file(path)
-when "2"
-  puts "Введите данные работника: "
-  data = input
-end
-
-emps = Array.new()
-for i in 0..data.length/11 - 1
-  emps<< TestEmployee.new(data[i*11..i*11+10])
-end
-emps.each{|el| puts el.employee}
+obj = TestEmployee.new
+obj.run("1")
